b91c7ad0ce65f7ee1d07d2ecf17ecce227cf6caa
Whitney Monk <whitneym92@gmail.com>
2020-08-01 18:30:43 -0500

    estab npm packages and connection to mysql

---
+++ employee.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require('console.table');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "employees_db"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+});
